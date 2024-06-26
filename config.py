b577723890b4813bf171c8403714a9f84b8cd44e
MilkHunter <51160128+DicKeeper@users.noreply.github.com>
2024-06-26 13:50:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    bot_token: str
+    telegram_token: str
     openai_api_key: str
 
     class Config:
         env_file = ".env"
-        env_file_encoding = 'utf-8'
 
 settings = Settings()
